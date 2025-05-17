9ea2fad0511a3acc440e72befb9d91b3f51245ee
Laurent Duplat <divers@distr-action.com>
2025-05-17 23:07:50 +0200

    fix: ajout export correct supabase

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,3 +1,6 @@
-declare module './supabaseClient' {
-  export const supabase: any;
-}
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://yuntqzgwicfxqrfpyrig.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1bnRxemd3aWNmeHFyZnB5cmlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1MDUxNjIsImV4cCI6MjA2MzA4MTE2Mn0.zks1E-PxcPriGmS5ObdHBuCXYoXi6frgYRNLPtFgzqg'; // Remplace par ta vraie clé anonyme !
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
